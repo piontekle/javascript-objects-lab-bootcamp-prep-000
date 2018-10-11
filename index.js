440123b1f644f9b3751de7ad933d92fa6db53f56
Lauren Piontek <piontekle@gmail.com>
2018-10-11 18:29:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue (object, key, value) {
-  object = Object.assign(object, {key: value});
+  object = Object.assign(object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value
   return object
 }
 
+function deleteFromObjectByKey (object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
+}
