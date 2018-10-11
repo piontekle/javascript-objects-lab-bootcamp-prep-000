9936aa6b0b5468bfe02aac5fe84c6cfb5a2dc0f6
Lauren Piontek <piontekle@gmail.com>
2018-10-11 18:34:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey (object, key) {
+function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj.key;
   return newObj;
 }
 
-function destructivelyDeleteFromObjectByKey (object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
 }
